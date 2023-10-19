b4e4e50bf2f57b672f7111691db7d4e0b259d1b3
Jobin Sebastian <jobsebastianjobin@gmail.com>
2023-10-19 16:51:31 +0530

    model class and json response

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-
 import 'package:apiweatherapp/screens/home_page.dart';
+import 'package:apiweatherapp/services/location_provider.dart';
+import 'package:apiweatherapp/services/weather_service_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,27 +14,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Weather App',
-      theme: ThemeData(
-
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.transparent,
-          elevation:0
-        ),
-
-       iconTheme: IconThemeData(
-         color: Colors.white
-       ),
-
-
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home:HomePage()
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => LocationProvider()),
+        ChangeNotifierProvider(create: (context) => WeatherServiceProvider())
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Weather App',
+          theme: ThemeData(
+            appBarTheme:
+                AppBarTheme(backgroundColor: Colors.transparent, elevation: 0),
+            iconTheme: IconThemeData(color: Colors.white),
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: HomePage()),
     );
   }
 }
-
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:apiweatherapp/data/image_path.dart';
+import 'package:apiweatherapp/services/location_provider.dart';
+import 'package:apiweatherapp/services/weather_service_provider.dart';
 import 'package:apiweatherapp/utils/apptext.dart';
 import 'package:apiweatherapp/utils/custom_divider_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,11 +14,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    Provider.of<LocationProvider>(context, listen: false).determinePosition();
 
-  bool _clicked=false;
+    Provider.of<WeatherServiceProvider>(context,listen: false).fetchWeatherDataByCity("Dubai");
+
+    super.initState();
+  }
+
+  bool _clicked = false;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+
+    final locationProvider = Provider.of<LocationProvider>(context);
+
     return Scaffold(
       backgroundColor: Colors.black,
       extendBodyBehindAppBar: true,
@@ -29,112 +43,121 @@ class _HomePageState extends State<HomePage> {
                 fit: BoxFit.cover, image: AssetImage(background[0]))),
         child: Stack(
           children: [
-
-   _clicked==true?         Positioned(
-            top: 50,
-              left: 20,
-              right: 20,
-              child: Container(
-                height: 45,
-
-                child: TextFormField(
-                  decoration: InputDecoration(
-                 enabledBorder: UnderlineInputBorder(
-                   borderSide: BorderSide(color: Colors.white),
-
-                 ),
-
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-
+            _clicked == true
+                ? Positioned(
+                    top: 50,
+                    left: 20,
+                    right: 20,
+                    child: Container(
+                      height: 45,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                        ),
+                      ),
                     ),
-
-                  ),
-
-                ),
-              ),
-            ):
-            SizedBox.shrink(),
-
+                  )
+                : SizedBox.shrink(),
             Container(
               height: 50,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.location_pin,
-                          color: Colors.red,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            AppText(
-                              data: "Dubai",
-                              color: Colors.white,
-                              fw: FontWeight.w700,
-                              size: 18,
-                            ),
-                            AppText(
-                              data: "Good Morning",
-                              color: Colors.white,
-                              fw: FontWeight.w400,
-                              size: 14,
-                            )
-                          ],
-                        ),
+              child: Consumer<LocationProvider>(
+                  builder: (context, locationProvider, child) {
+                var locationCity;
+                if (locationProvider.currentLocationName != null) {
+                  locationCity = locationProvider.currentLocationName!.locality;
+                } else {
+                  locationCity = "Unknown Location";
+                }
 
-
-                      ],
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.location_pin,
+                            color: Colors.red,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              AppText(
+                                data: locationCity,
+                                color: Colors.white,
+                                fw: FontWeight.w700,
+                                size: 18,
+                              ),
+                              AppText(
+                                data: "Good Morning",
+                                color: Colors.white,
+                                fw: FontWeight.w400,
+                                size: 14,
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-
-                  IconButton(onPressed: (){
-
-                    setState(() {
-                      _clicked=!_clicked;
-                    });
-
-                  }, icon: Icon(Icons.search,size: 32,))
-                ],
-              ),
+                    IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _clicked = !_clicked;
+                          });
+                        },
+                        icon: Icon(
+                          Icons.search,
+                          size: 32,
+                        ))
+                  ],
+                );
+              }),
             ),
-
-
             Align(
-              alignment: Alignment(0,-0.7),
+                alignment: Alignment(0, -0.7),
                 child: Image.asset(imagePath[6])),
-
             Align(
-              alignment: Alignment(0,0),
+              alignment: Alignment(0, 0),
               child: Container(
                 height: 130,
-
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    AppText(data: "21 C",color: Colors.white,fw: FontWeight.bold,size:32,),
-                    AppText(data: "Snow",color: Colors.white,fw: FontWeight.w600,size: 26,),
-
-                    AppText(data: DateTime.now().toString(),color: Colors.white,)
+                    AppText(
+                      data: "21 C",
+                      color: Colors.white,
+                      fw: FontWeight.bold,
+                      size: 32,
+                    ),
+                    AppText(
+                      data: "Snow",
+                      color: Colors.white,
+                      fw: FontWeight.w600,
+                      size: 26,
+                    ),
+                    AppText(
+                      data: DateTime.now().toString(),
+                      color: Colors.white,
+                    )
                   ],
                 ),
               ),
             ),
-
             Align(
-              alignment: Alignment(0.0,0.75),
+              alignment: Alignment(0.0, 0.75),
               child: Container(
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.black.withOpacity(0.4)
-                ),
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.black.withOpacity(0.4)),
                 height: 180,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -144,36 +167,59 @@ class _HomePageState extends State<HomePage> {
                       children: [
                         Row(
                           children: [
-                            Image.asset('assets/img/temperature-high.png',height: 55,)
-                            ,Column(
+                            Image.asset(
+                              'assets/img/temperature-high.png',
+                              height: 55,
+                            ),
+                            Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-
-                                AppText(data: "Temp Max",color: Colors.white,size: 14,fw: FontWeight.w600,),
-                                AppText(data: "21C",color: Colors.white,size: 14,fw: FontWeight.w600,)
-
+                                AppText(
+                                  data: "Temp Max",
+                                  color: Colors.white,
+                                  size: 14,
+                                  fw: FontWeight.w600,
+                                ),
+                                AppText(
+                                  data: "21C",
+                                  color: Colors.white,
+                                  size: 14,
+                                  fw: FontWeight.w600,
+                                )
                               ],
                             )
                           ],
                         ),
-                        SizedBox(width: 20,),
+                        SizedBox(
+                          width: 20,
+                        ),
                         Row(
                           children: [
-                            Image.asset('assets/img/temperature-low.png',height: 55,)
-                            ,Column(
+                            Image.asset(
+                              'assets/img/temperature-low.png',
+                              height: 55,
+                            ),
+                            Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-
-                                AppText(data: "Temp Min",color: Colors.white,size: 14,fw: FontWeight.w600,),
-                                AppText(data: "21C",color: Colors.white,size: 14,fw: FontWeight.w600,)
-
+                                AppText(
+                                  data: "Temp Min",
+                                  color: Colors.white,
+                                  size: 14,
+                                  fw: FontWeight.w600,
+                                ),
+                                AppText(
+                                  data: "21C",
+                                  color: Colors.white,
+                                  size: 14,
+                                  fw: FontWeight.w600,
+                                )
                               ],
                             )
                           ],
                         ),
-
                       ],
                     ),
                     CustomDivider(
@@ -182,51 +228,70 @@ class _HomePageState extends State<HomePage> {
                       color: Colors.white,
                       thickness: 2,
                     ),
-
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Row(
                           children: [
-                            Image.asset('assets/img/sun.png',height: 55,)
-                            ,Column(
+                            Image.asset(
+                              'assets/img/sun.png',
+                              height: 55,
+                            ),
+                            Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-
-                                AppText(data: "Sunrise",color: Colors.white,size: 14,fw: FontWeight.w600,),
-                                AppText(data: "21C",color: Colors.white,size: 14,fw: FontWeight.w600,)
-
+                                AppText(
+                                  data: "Sunrise",
+                                  color: Colors.white,
+                                  size: 14,
+                                  fw: FontWeight.w600,
+                                ),
+                                AppText(
+                                  data: "21C",
+                                  color: Colors.white,
+                                  size: 14,
+                                  fw: FontWeight.w600,
+                                )
                               ],
                             )
                           ],
                         ),
-                        SizedBox(width: 20,),
+                        SizedBox(
+                          width: 20,
+                        ),
                         Row(
                           children: [
-                            Image.asset('assets/img/moon.png',height: 55,)
-                            ,Column(
+                            Image.asset(
+                              'assets/img/moon.png',
+                              height: 55,
+                            ),
+                            Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-
-                                AppText(data: "Sunset",color: Colors.white,size: 14,fw: FontWeight.w600,),
-                                AppText(data: "21C",color: Colors.white,size: 14,fw: FontWeight.w600,)
-
+                                AppText(
+                                  data: "Sunset",
+                                  color: Colors.white,
+                                  size: 14,
+                                  fw: FontWeight.w600,
+                                ),
+                                AppText(
+                                  data: "21C",
+                                  color: Colors.white,
+                                  size: 14,
+                                  fw: FontWeight.w600,
+                                )
                               ],
                             )
                           ],
                         ),
-
                       ],
                     ),
-
                   ],
                 ),
               ),
             )
-
-
           ],
         ),
       ),
